88936a3ed2e4762f533b75465c61eb8897ba3c63
Advait <advait980@gmail.com>
2018-02-24 19:41:57 +0530

    Initial commit

---
+++ src/Login.java
@@ -0,0 +1,37 @@
+
+
+
+
+/**
+ * Servlet implementation class Login
+ */
+
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		System.out.println("INSIDE POST METHOD");
+	}
+	Added new line on 24-Feb-2018
+
+}
